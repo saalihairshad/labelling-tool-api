c4dc3188a2cab807de8b87d564c27350e4264313
saalihairshad <saalihairshad@gmail.com>
2020-04-14 04:01:31 +0200

    directory structure change

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ mongoos
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.log("Could not connect to MongoDB..."));
 
-
-
-
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
